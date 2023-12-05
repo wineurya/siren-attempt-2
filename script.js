6d70a8d22b33101f64b47e45763e117d229b1751
Wineury <walmont2@students.kennesaw.edu>
2023-12-04 21:31:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('#nav-links a').forEach(link => {
+    link.addEventListener('mouseover', () => {
+        // Add hover effects
+    });
+
+    link.addEventListener('mouseout', () => {
+        // Remove hover effects
+    });
+});
